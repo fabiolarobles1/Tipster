a2e330377c642a7228d7565b5f929684ca9a93d5
Fabiola E. Robles Vega <fabiolarobles19@fb.com>
2020-06-25 18:08:34 -0400

    Completed main features

--- Tipster/ViewController.m
+++ Tipster/ViewController.m
@@ -10,6 +10,14 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *billTextField;
+
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
+
 @end
 
 @implementation ViewController
@@ -17,6 +25,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+}
+
+- (IBAction)onTap:(id)sender {
+    NSLog(@"Hello");
+    //Hiding Keyboard on tap
+    [self.view endEditing:YES];
+}
+
+- (IBAction)onEdit:(id)sender {
+    double bill = [self.billTextField.text doubleValue];
+    
+    NSArray *percentages = @[@(0.15),@(0.2),@(0.22)];
+    
+    double tipPercentage = [percentages[self.tipControl.selectedSegmentIndex]doubleValue];
+    double tip = tipPercentage*bill;
+    double total = bill+tip;
+    self.tipLabel.text = [NSString stringWithFormat:@"$ %.2f", tip];
+    self.totalLabel.text = [NSString stringWithFormat:@"$ %.2f", total];
 }
 
 
